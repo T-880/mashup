cb90f557bc4d930e41912d69f2be970810923cab
Trella <mr.trella@hotmail.com>
2026-03-12 10:50:17 +0100

    Lagt till leafletkarta med markerfix och CSS

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import "../scss/main.scss";
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+
+import markerIcon2x from 'leaflet/dist/images/marker-icon-2x.png';
+import markerIcon from 'leaflet/dist/images/marker-icon.png';
+import markerShadow from 'leaflet/dist/images/marker-shadow.png';
+
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow
+});
+
+const map = L.map('mapContainer').setView([55.6050, 13.0038], 9);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; OpenStreetMap contributors'
+}).addTo(map);
